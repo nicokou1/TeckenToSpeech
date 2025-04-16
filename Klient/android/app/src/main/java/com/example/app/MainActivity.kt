1e33c74f911fd0bc1b5bae2703238dc6fd65a779
DivineMoonlight <mimozaramona1@gmail.com>
2025-04-16 15:13:00 +0200

    andra test

--- Klient/android/app/src/main/java/com/example/app/MainActivity.kt
+++ Klient/android/app/src/main/java/com/example/app/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
 
+// NEJ NEJ
 
 class MainActivity : ComponentActivity() {
 
