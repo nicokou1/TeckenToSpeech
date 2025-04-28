77ae533febfc2927a745d773efdc5c45b3cce240
farzaneibrahim <farzane.ibrahimi@gmail.com>
2025-04-28 15:24:47 +0200

    bakgrund

--- Klient/android/app/src/main/java/com/example/app/MainActivity.kt
+++ Klient/android/app/src/main/java/com/example/app/MainActivity.kt
@@ -3,35 +3,23 @@ package com.example.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.app.connection.*
-import com.example.app.buttons.*
-
-/**
- * MainActivity is the primary entry point of the application.
- * It is responsible for initializing the UI and setting up necessary components during app startup
- * Also serves as a gateway to the business logic.
- * @author Mimoza Behrami & Farzaneh Ibrahimi
- * @since 2025-04-14
- */
-
-// Changelog:
-// 2025-04-17 Mimoza Behrami - Lagt till JavaDoc
-// 2025-04-24 Mimoza Behrami - Lagt till knapparna från Buttons i onCreate()
+import com.example.app.buttons.* // För dina knappar
+import com.example.app.connection.ConnectionComposable
 
 class MainActivity : ComponentActivity() {
 
     var connection: ConnectionComposable = ConnectionComposable()
 
-    //onCreate är alltid det första som körs då appen öppnas
+    // onCreate är alltid det första som körs då appen öppnas
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -44,10 +32,15 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(innerPadding)
                     ) {
-                        // visar logiken från ConnectionComposable
-                        //connection.ShowLetterOnScreen()
+                        // Här lägger vi till bakgrundsbilden
+                        Image(
+                            painter = painterResource(id = R.drawable.img),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
 
-                        // ställer in och visar knapparna
+                        // Ställer in och visar knapparna
                         Row(
                             modifier = Modifier
                                 .align(Alignment.BottomCenter)
