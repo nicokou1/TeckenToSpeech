4cb4eb1f40197c454c325a427fd3bfbd6d43ed3e
Viktor Vallmark <vallmark.viktor@gmail.com>
2025-05-01 15:54:20 +0200

    added a python script to produce frames and start a shared memory region for IPC
    Signed-off-by: Viktor Vallmark <vallmark.viktor@gmail.com>

--- Klient/android/app/src/main/java/com/example/app/PostFetcher.kt
+++ Klient/android/app/src/main/java/com/example/app/PostFetcher.kt
@@ -2,7 +2,7 @@ package com.example.app
 
 // 2025-04-14
 // mimoza har lagt till:
-// Testar att hämta data från ett test-API
+// Testar att h?mta data fr?n ett test-API
 
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -12,24 +12,22 @@ import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
-// suspend-funktioner används för tidskrävande uppgifter, som nätverksanslutning
-// måste köras i en coroutine (lättare alternativ till java.Thread)
+// suspend-funktioner anv?nds f?r tidskr?vande uppgifter, som n?tverksanslutning
+// m?ste k?ras i en coroutine (l?ttare alternativ till java.Thread)
 // returnerar datatypen List<Post>
 
 suspend fun fetchPosts(): List<Post> {
-
-    //skapar HTTP-klient med en CIO-motor
-    val client = HttpClient(CIO) {
-
-        //klientdriven innehållsförhandling (HTTP Header "accept: application/json")
-        install(ContentNegotiation) {
-
-            // ignorerar fält i JSON som inte finns som instansvariabler i Post
-            json(Json { ignoreUnknownKeys = true })
+    // skapar HTTP-klient med en CIO-motor
+    val client =
+        HttpClient(CIO) {
+            // klientdriven inneh?llsf?rhandling (HTTP Header "accept: application/json")
+            install(ContentNegotiation) {
+                // ignorerar f?lt i JSON som inte finns som instansvariabler i Post
+                json(Json { ignoreUnknownKeys = true })
+            }
         }
-    }
 
-    // klienten anropar GET och får ett HTTP Response-objekt som innehåller rådata
+    // klienten anropar GET och f?r ett HTTP Response-objekt som inneh?ller r?data
     // .body() omvandlar datan till List<Post>
     return client.get("http://10.2.0.95:8000/app").body()
 }
